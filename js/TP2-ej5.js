5f86af75a084b9710c89ee95ed52e5cdfb534565
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-04 09:59:29 -0300

    ejercicio 5 a medias

---
+++ js/TP2-ej5.js
@@ -0,0 +1,59 @@
+let dni=prompt('ingrese su DNI sin puntos ni espacios');
+document.write(`D.N.I ${dni}`);
+let div=parseInt(23/dni);
+console.log(div)
+
+switch(div){
+    case 1:
+        document.write('corresponde la letra A');
+    case div===2 :
+        document.write('corresponde la letra B');
+    case div===3:
+        document.write('corresponde la letra C');
+    case div===4:
+        document.write('corresponde la letra D');
+    case 5:
+        document.write('corresponde la letra E');
+    case div===6:
+        document.write('corresponde la letra F');
+    case div===7:
+        document.write('corresponde la letra G');
+    case div===8:
+        document.write('corresponde la letra H');
+    case div===9:
+        document.write('corresponde la letra I');
+    case '10':
+        document.write('corresponde la letra J');
+    case '11':
+        document.write('corresponde la letra K');
+    case '12':
+        document.write('corresponde la letra L');
+    case '13':
+        document.write('corresponde la letra M');
+    case '14':
+        document.write('corresponde la letra N');
+    case '15':
+        document.write('corresponde la letra O');
+    case '16':
+        document.write('corresponde la letra P');
+    case '17':
+        document.write('corresponde la letra Q');
+    case '18':
+        document.write('corresponde la letra R');
+    case '19':
+        document.write('corresponde la letra S');
+    case '20':
+        document.write('corresponde la letra T');
+    case '21':
+        document.write('corresponde la letra U');
+    case '22':
+        document.write('corresponde la letra V');
+    case '23':
+        document.write('corresponde la letra W');
+    case '24':
+        document.write('corresponde la letra X');
+    case '25':
+        document.write('corresponde la letra Y');
+    case '26':
+        document.write('corresponde la letra Z');
+}

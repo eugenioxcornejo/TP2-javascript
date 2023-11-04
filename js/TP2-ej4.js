15be60a33b53b0c193342a01fcc939b0714b239d
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-04 09:05:50 -0300

    ejerciocio4 se agrego a modo de prueba cartel no numerico

--- js/TP2-ej4.js
+++ js/TP2-ej4.js
@@ -1,14 +1,21 @@
 // 4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
 
-do{let primernumero= parseFloat(prompt("ingrese un numero"));
+let suma = parseFloat(0);
+do{
+let primernumero= parseFloat(prompt("ingrese un numero"));
 console.log(primernumero);
-let numero2=parseFloat(prompt("ingrese un numero2"));
 
-    do{
-    let suma=(primernumero+numero2);
-console.log(suma)}
-    while (confirm("desea agregar otro numero?"))
-    
-     
-} while(primernumero && numero2 !== null){
-    alert("ingrese un numero")}
+
+
+suma=(suma + primernumero);
+console.log(suma);
+
+
+}
+
+while(!isNaN(suma)&& confirm(`desea agregar otro numero`));
+
+if(isNaN(suma))
+{alert('valor ingresado incorrecto')}
+else{document.write('El valor ingresado no es numerico no puede realizarce la suma')}
+document.write(`El Total de la suma de los numeros ingresados es ${suma}`);  

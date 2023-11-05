19e5bef68092f2a967aacfde39492814f8c5710b
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-05 09:52:27 -0300

    ejercicio 5 switch no responde

--- js/TP2-ej5.js
+++ js/TP2-ej5.js
@@ -1,59 +1,78 @@
-let dni=prompt('ingrese su DNI sin puntos ni espacios');
+do{let dni=prompt('ingrese su DNI sin puntos ni espacios');
 document.write(`D.N.I ${dni}`);
-let div=parseInt(23/dni);
-console.log(div)
+let div=(dni%23);
+console.log(div);
 
 switch(div){
-    case 1:
-        document.write('corresponde la letra A');
-    case div===2 :
-        document.write('corresponde la letra B');
-    case div===3:
-        document.write('corresponde la letra C');
-    case div===4:
-        document.write('corresponde la letra D');
-    case 5:
-        document.write('corresponde la letra E');
-    case div===6:
-        document.write('corresponde la letra F');
-    case div===7:
-        document.write('corresponde la letra G');
-    case div===8:
-        document.write('corresponde la letra H');
-    case div===9:
-        document.write('corresponde la letra I');
-    case '10':
-        document.write('corresponde la letra J');
-    case '11':
-        document.write('corresponde la letra K');
-    case '12':
-        document.write('corresponde la letra L');
-    case '13':
-        document.write('corresponde la letra M');
-    case '14':
-        document.write('corresponde la letra N');
-    case '15':
-        document.write('corresponde la letra O');
-    case '16':
-        document.write('corresponde la letra P');
-    case '17':
-        document.write('corresponde la letra Q');
-    case '18':
-        document.write('corresponde la letra R');
-    case '19':
-        document.write('corresponde la letra S');
-    case '20':
+    case '0':
         document.write('corresponde la letra T');
-    case '21':
-        document.write('corresponde la letra U');
-    case '22':
-        document.write('corresponde la letra V');
-    case '23':
+        break;
+    case '1':
+        document.write('corresponde la letra R');
+        break;
+    case '2':
         document.write('corresponde la letra W');
-    case '24':
-        document.write('corresponde la letra X');
-    case '25':
+        break;
+    case '3':
+        document.write('corresponde la letra A');
+        break;
+    case '4':
+        document.write('corresponde la letra G');
+        break;
+    case '5':
+        document.write('corresponde la letra M');
+        break;
+    case '6':
         document.write('corresponde la letra Y');
-    case '26':
+        break
+    case '7':
+        document.write('corresponde la letra F');
+        break;
+    case '8':
+        document.write('corresponde la letra P');
+        break
+    case '9':
+        document.write('corresponde la letra D');
+        break;
+    case '10':
+        document.write('corresponde la letra X');
+        break;
+    case '11':
+        document.write('corresponde la letra B');
+        break;
+    case '12':
+        document.write('corresponde la letra N');
+        break;
+    case '13':
+        document.write('corresponde la letra J');
+        break;
+    case '14':
         document.write('corresponde la letra Z');
-}
+        break;
+    case '15':
+        document.write('corresponde la letra S');
+        break;
+    case '16':
+        document.write('corresponde la letra Q');
+        break;
+    case '17':
+        document.write('corresponde la letra V');
+        break;
+    case '18':
+        document.write('corresponde la letra H');
+        break;
+    case '19':
+        document.write(`corresponde la letra L`);
+        console.log(`letra L`)
+        break;
+    case '20':
+        document.write('corresponde la letra C');
+        break;
+    case '21':
+        document.write('corresponde la letra K');
+        break;
+    case '22':
+        document.write('corresponde la letra E');
+        break;
+   
+}} while(confirm('desea ingresar otro D.N.I?'));

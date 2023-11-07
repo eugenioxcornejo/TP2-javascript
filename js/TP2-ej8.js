bf8b01106ba745b8fb8bc9b22bfba250e2f4f16e
eugenioxcornejo <89100297+eugenioxcornejo@users.noreply.github.com>
2023-11-07 15:59:11 -0300

    ejercicio 8 completo

---
+++ js/TP2-ej8.js
@@ -0,0 +1,13 @@
+
+let numero=parseInt(prompt("ingrese un numero entre 1 y 50"));
+if(numero>50){
+    document.write('numero mayor a 50 por favor indique un numero entre 1 y 50');
+    }
+
+    for (numero; numero<=50; numero++){
+        for(let columna=1; columna<=numero; columna++){
+            document.write(`${columna}  `); 
+        }
+        
+        document.write(`<p></p>`)
+    }
